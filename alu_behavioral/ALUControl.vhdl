e0f64b6834aede4da607a2f9d8cf3446b578fa0b
Gaby-JBR <136042243+Gaby-JBR@users.noreply.github.com>
2023-10-22 22:18:25 -0600

    Update ALUControl.vhdl

--- alu_behavioral/ALUControl.vhdl
+++ alu_behavioral/ALUControl.vhdl
@@ -16,16 +16,8 @@ begin
               "0000" when (ALUOp = "10") and (FuncCode = "100100") else
               "0001" when (ALUOp = "10") and (FuncCode = "100101") else
               "0111" when (ALUOp = "10") and (FuncCode = "101010") else
+	          "1000" when (ALUOp = "10") and (FuncCode = "000000") else
               "1111";
 
---    with ALUOp&FuncCode select
---        ALUCtl <= "0010" when "00------",
---                 "0110" when "-1------",
---                  "0010" when "1---0000",
---                  "0110" when "1---0010",
---                  "0000" when "1---0100",
---                  "0001" when "1---0101",
---                  "0111" when "1---1010",
---                  "1111" when others;
 
 end comportamiento;
